c9bd2aa485a176258daea7d176abba684b276a07
Jacob Cho <jacobshcho@gmail.com>
2014-07-18 18:34:28 -0700

    Rotten Tomatoes
    Simple iOS app displaying new movies and their ratings

---
+++ TableViewController.h
@@ -0,0 +1,13 @@
+//
+//  TableViewController.h
+//  Rotten Tomatoes
+//
+//  Created by Jacob Cho on 2014-07-18.
+//  Copyright (c) 2014 Jacob Cho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewController : UITableViewController
+
+@end
