d3a514b34b7155f915ead82c767cfb9f201b633a
Jacob Cho <jacobshcho@gmail.com>
2014-07-18 18:26:37 -0700

    Initial Commit

---
+++ Rotten Tomatoes/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Rotten Tomatoes
+//
+//  Created by Jacob Cho on 2014-07-18.
+//  Copyright (c) 2014 Jacob Cho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Rotten Tomatoes/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Rotten Tomatoes
+//
+//  Created by Jacob Cho on 2014-07-18.
+//  Copyright (c) 2014 Jacob Cho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
